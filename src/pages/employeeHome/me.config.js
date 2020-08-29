0ac256e627a7f31d772357788fc925fea7da4195
stevekeol <stevekeol.x@gmail.com>
2020-08-29 23:54:17 +0800

    feat: employeeHome

---
+++ src/pages/employeeHome/me.config.js
@@ -0,0 +1,8 @@
+export default {
+
+  // 声明需要引入的第三方组件(页面中组件名需要小写)
+  // usingComponents: {
+  //   'navbar': '../../components/navbar/navbar'
+  // }  
+
+}
---
+++ src/pages/employeeHome/me.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { View, Button, Text, Image } from '@tarojs/components'
+
+
+import './me.scss'
+
+//先集中导入colorUI的样式
+import '../../components/icon.wxss' //为什么此处导入的icon集合，在me.jsx中也能用
+import '../../components/main.wxss'
+
+import Navbar from '../../components/navbar/navbar'
+
+import level_diamond from '../../images/level_diamond.png'
+import avator from '../../images/avator.jpg'
+
+class Me extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }  
+  
+  componentDidMount() {
+    // console.log('fuck')
+  }
+  componentDidShow() {}
+  componentDidHide() {}
+
+  render () {
+    return (
+      <View>
+        <Navbar bgColor="bg-blue" content="张杰"></Navbar>
+        <View class="backgroundHead">
+          <Image class="avatar" src={avator} mode="scaleToFill"></Image>
+        </View>
+
+        <View class="body margin-top-xl">
+          <View class="title">身份标识</View>
+          <View class="memberCard" bindtap="enterMemberCardInfo">
+            <View class="memberCardInfo">
+              <View class="store">尚宸装饰</View>
+              <View class="number">No.10010123</View>
+            </View>
+            <View class="memberCardLevel">
+              <View class="icon">
+                <Image src={ level_diamond }></Image>
+                <View>★★★★</View>
+              </View>
+            </View>
+          </View>
+          <View class="title margin-top-xl">其它</View>
+          <View class="tableContainer margin-top">
+            <View class="item">
+              <View class="name cuIcon-locationfill">业绩详情</View>
+              <View class="cuIcon-right"></View>
+            </View>
+            <View class="item">
+              <View class="name cuIcon-mobilefill">工作日程</View>
+              <View class="cuIcon-right"></View>
+            </View>
+            <View class="item" bindtap="enterAllOrders">
+              <View class="name cuIcon-timefill">数据导出</View>
+              <View class="cuIcon-right"></View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default Me
